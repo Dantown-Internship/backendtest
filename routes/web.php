1bd1ec3e880eb7f251f0a43bc73366f0cd24d8cd
Joel Okoromi <okmarq@gmail.com>
2024-03-13 22:56:27 +0100

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\WalletController;
+use App\Models\SystemPool;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,17 +18,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('welcome')->with('system_pool', SystemPool::where('name', config('constant.system_pool.name'))->first());
+})->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/wallet', function () {
+    return view('wallet')->with('user', auth()->user());
+})->middleware(['auth', 'verified'])->name('wallet');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/wallet', [WalletController::class, 'index'])->name('wallet');
+
+    Route::get('/transaction', [TransactionController::class, 'index'])->name('transaction');
+});
+
+Route::middleware('maker')->group(function () {
+    Route::get('/transaction/create', [TransactionController::class, 'create'])->name('transaction.create');
+    Route::post('/transaction/create', [TransactionController::class, 'store'])->name('transaction.store');
+    Route::get('/transaction/edit/{transaction}', [TransactionController::class, 'edit'])->name('transaction.edit');
+    Route::post('/transaction/edit/{transaction}', [TransactionController::class, 'update'])->name('transaction.update');
+});
+
+Route::middleware('checker')->group(function () {
+    Route::get('/transaction/review/{transaction}', [TransactionController::class, 'review'])->name('transaction.review');
+    Route::post('/transaction/review/{transaction}', [TransactionController::class, 'decide'])->name('transaction.review');
 });
 
 require __DIR__.'/auth.php';
