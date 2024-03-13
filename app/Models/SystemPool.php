473c010530f35768186c06de64c2ba12e77487b8
Joel Okoromi <okmarq@gmail.com>
2024-03-13 22:54:31 +0100

    make models

---
+++ app/Models/SystemPool.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SystemPool extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'balance'];
+    
+    public function creditBalance(float $amount)
+    {
+        $this->increment('balance', $amount);
+    }
+
+    public function debitBalance(float $amount)
+    {
+        $this->decrement
+        ('balance', $amount);
+    }
+}
---
+++ app/Models/Wallet.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Wallet extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'balance'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // banking uses credit to add to balance and debit to deduct from balance
+    // accounting uses debit to add to balance and credit to deduct from balance
+    public function creditBalance(float $amount)
+    {
+        $this->increment('balance', $amount);
+    }
+
+    public function debitBalance(float $amount)
+    {
+        $this->decrement
+        ('balance', $amount);
+    }
+}
