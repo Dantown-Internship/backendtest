071911efae6adde201bd7fab88729959400cde55
Joel Okoromi <okmarq@gmail.com>
2024-03-13 22:55:45 +0100

    configure service providers

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Gate;
+use App\Models\Transaction;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +23,12 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('edit-transaction', function ($user, Transaction $transaction) {
+            return $transaction->status === config('constant.status.rejected') ? Response::allow() : Response::deny('Transaction cannot be edited because it is already approved or pending.');
+        });
+        
+        Gate::define('review-transaction', function ($user, Transaction $transaction) {
+            return $transaction->status === config('constant.status.pending') ? Response::allow() : Response::deny('Transaction cannot be reviewed because it is already approved or rejected.');
+        });
     }
 }
